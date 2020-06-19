9cb429a576fd6756952871387ecdf9620d971052
Karl Hallsby <karl@hallsby.com>
2020-06-19 16:32:38 +0200

    Back to mainline 20.03 channel, and only import nixos-unstable for mu

--- modules/email.nix
+++ modules/email.nix
@@ -5,6 +5,8 @@
     msmtp # For sending mail using SMTP
     isync # Project name. Binary is mbsync
     # unstable.mu
-    mu
+    (import (fetchTarball "channel:nixos-unstable") {config = config.nixpkgs.config;}).mu
+    # This one-liner is only used until NixOS bumps the version of mu to 1.4.x
+    # The config = ... Ensures changes made to packages in the standard nixpkgs carry through
   ];
 }
