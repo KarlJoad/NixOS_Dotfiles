52493f949af1605f3d28891e9b422f516a35f2b6
Karl Hallsby <karl@hallsby.com>
2020-06-19 16:48:01 +0200

    Move all system-level configurations to single set

--- configuration.nix
+++ configuration.nix
@@ -162,17 +162,18 @@
     hashedPassword = "$6$SP0uXGjZaunNycZ$B7Yt8sdT26cq3Na0pfoGvE36De7cFdzP63JvbtV6myPglK4.LY1w/jFlbnkH9nCNR7qj8/ZztYTrzQYcUb9Ac1";
   };
 
-  system.autoUpgrade = {
-    enable = true;
-    allowReboot = false;
+  system = {
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "20.03"; # Did you read the comment?
+    autoUpgrade = {
+      enable = true;
+      allowReboot = false;
+      channel = "https://channels.nixos.org/nixos-unstable/";
+    };
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
-  system.autoUpgrade.channel = "https://channels.nixos.org/nixos-unstable/";
 }
