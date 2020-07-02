2a8a448e07aca63a52b8fd89d6167fbe264e74f2
Karl Hallsby <karl@hallsby.com>
2020-07-02 15:49:50 +0200

    Attempt to make KDE manage my wallpaper, which doesn't really work.

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -15,6 +15,16 @@
     # Enable the KDE Desktop Environment
     desktopManager = {
       plasma5.enable = true;
+      wallpaper = {
+        combineScreens = false;
+        mode = "center";
+        # The file ~/.background-image is used as a background image.
+        # "center": Center the image on the background. If it is too small, it will be surrounded by a black border.
+        # "fill": Like scale, but preserves aspect ratio by zooming the image until it fits. Either a horizontal or a vertical part of the image will be cut off.
+        # "max": Like fill, but scale the image to the maximum size that fits the screen with black borders on one side.
+        # "scale": Fit the file into the background without repeating it, cutting off stuff or using borders. But the aspect ratio is not preserved either.
+        # "tile": Tile (repeat) the image in case it is too small for the screen.
+      };
     };
     # Enable the xmonad and i3 Tiling window managers.
     windowManager = {
