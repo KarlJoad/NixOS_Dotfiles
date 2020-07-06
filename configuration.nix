80376e39a141bda2eac618d859ae4dca694c8308
Karl Hallsby <karl@hallsby.com>
2020-07-07 00:23:54 +0200

    Move global useDHCP into networking block
    This is just to make things a little bit clearer for the next time I
    use configuration.nix on another computer.

--- configuration.nix
+++ configuration.nix
@@ -64,14 +64,11 @@
   };
 
   networking = {
+    # Global useDHCP flag is deprecated, therefore explicitly set to false here.
+    # Per-interface useDHCP will be mandatory in the future.
+    useDHCP = false;
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
   };
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
