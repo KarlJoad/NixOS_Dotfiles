bd4ff5edfebeddf30d976e339746de58a2c13c9b
Karl Hallsby <karl@hallsby.com>
2020-07-14 13:22:40 -0500

    Add pygmentize for coloring source code in TeX/LaTeX docs

--- modules/doc-prep.nix
+++ modules/doc-prep.nix
@@ -4,6 +4,9 @@
   environment.systemPackages = with pkgs; [
     libreoffice-fresh-unwrapped
     texlive.combined.scheme-full
+    pygmentex
+    python38Packages.pygments # Needed for pygmentize
+    python38Packages.pygments-markdown-lexer
     texstudio # Nice GUI for editing TeX stuff.
     gimp inkscape # Image creation and manipulation (Raster and Vector).
 
