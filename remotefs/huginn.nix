8806bbf216dfb2395475a0a8afe3a60f65cac12b
Karl Hallsby <karl@hallsby.com>
2020-08-10 22:15:58 -0500

    Add additional mount points for Huginn
    To access these types of mount points, you must navigate to
    /mnt/<server>/<share> manually the first time.
    Meaning, you must descend the /mnt directory hierarchy. After that,
    the shortcuts on the side of Dolphin will work the expected way.

--- remotefs/huginn.nix
+++ remotefs/huginn.nix
@@ -16,4 +16,20 @@
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=default,iocharset=utf8,uid=1000,gid=100,rw,file_mode=0644,dir_mode=0755,nounix,nobrl";
     in ["${automount_opts},credentials=/root/.homeSMBcredentials"];
   };
+
+  fileSystems."/mnt/Huginn/Processed" = {
+    device = "//192.168.20.10/Processed";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=default,iocharset=utf8,uid=1000,gid=100,rw,file_mode=0644,dir_mode=0755,nounix,nobrl";
+    in ["${automount_opts},credentials=/root/.homeSMBcredentials"];
+  };
+
+  fileSystems."/mnt/Huginn/Recording" = {
+    device = "//192.168.20.10/Recording";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=default,iocharset=utf8,uid=1000,gid=100,rw,file_mode=0644,dir_mode=0755,nounix,nobrl";
+    in ["${automount_opts},credentials=/root/.homeSMBcredentials"];
+  };
 }
