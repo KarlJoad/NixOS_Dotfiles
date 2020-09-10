f839edbf88b08d70fc1ff1bd1c2c2d8f027f2468
Karl Hallsby <karl@hallsby.com>
2020-09-10 18:53:45 -0500

    Add pandoc to document preparation system

--- modules/doc-prep.nix
+++ modules/doc-prep.nix
@@ -10,6 +10,7 @@
     python38Packages.pygments-markdown-lexer
     texstudio # Nice GUI for editing TeX stuff.
     gimp inkscape # Image creation and manipulation (Raster and Vector).
+    pandoc # Plain-text converter
 
     # PDF viewers and editor
     okular pdfpc zathura pdftk
