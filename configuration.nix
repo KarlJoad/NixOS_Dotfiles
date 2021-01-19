adea7e4d70f3265aad66fd6b6494276f5a57fdc2
Karl Hallsby <karl@hallsby.com>
2021-01-18 20:52:55 -0600

    import the home-manager module/plug-in for NixOS

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      <home-manager/nixos>
       ./modules/fs.nix
       ./modules/graphical.nix
       ./modules/email.nix
