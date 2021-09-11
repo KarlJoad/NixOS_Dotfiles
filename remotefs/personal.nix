1e43220c9348160c88cc9dff35061da25ac53054
Karl Hallsby <karl@hallsby.com>
2021-09-10 21:26:37 -0500

    Fix personal NAS automounting entry

--- remotefs/personal.nix
+++ remotefs/personal.nix
@@ -2,8 +2,10 @@
 
 {
   fileSystems."/mnt/NAS" = {
-    device = "192.168.1.5:/mnt/store";
-    fsType = "nfs";
-    options = [ "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60" ];
+    device = "//192.168.1.5/Store";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,vers=default,iocharset=utf8,uid=1000,gid=100,rw,file_mode=0644,dir_mode=0755,nounix,nobrl";
+    in ["${automount_opts},credentials=/root/.personalSMBcredentials"];
   };
 }
