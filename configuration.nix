2cbcd287c25048b4076959ef5fe4754137c641a9
Karl Hallsby <karl@hallsby.com>
2021-12-25 21:58:50 -0600

    Add configuration to handle flashing my ZSA Moonlander keyboard

--- configuration.nix
+++ configuration.nix
@@ -220,6 +220,7 @@
                     "libvirtd"
                     "networkmanager"
                     "video"
+                    "plugdev" # For keyboard configuration
                   ];
     packages = with pkgs; [ ];
     shell = "${pkgs.zsh}/bin/zsh";
@@ -236,6 +237,20 @@
     install = true; # Whether to create the emacs server user service
     enable = true; # Whether to start the emacs server user service
   };
+
+  # Should really be in /etc/udev/rules.d/50-wally.rules
+  services.udev.extraRules = ''
+    # Teensy rules for the Ergodox EZ
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+    ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+    KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+    # STM32 rules for the Moonlander and Planck EZ
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", \
+    MODE:="0666", \
+    SYMLINK+="stm32_dfu"
+  '';
 
   system = {
     # This value determines the NixOS release from which the default
